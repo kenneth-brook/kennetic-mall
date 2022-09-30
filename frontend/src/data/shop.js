9a031d1bda3387c48db7a31d8081dcbad029279d
Kenneth Brook <kenneth.brook@hotmail.com>
2022-09-30 18:36:04 -0400

    hex cat made

---
+++ frontend/src/data/shop.js
@@ -0,0 +1,8 @@
+const shop = [
+    {
+        id: 1,
+        shopName: 'Pancake Cat',
+        img: '../img/homeHead.jpg',
+        shopDesc: 'Pancake Cat is the purrrrrfect place to find all your meme-tastic gear'
+    },
+]
--- frontend/src/mall-home/Mall.js
+++ frontend/src/mall-home/Mall.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-import mallHeadBackground from "../img/homeHead.jpg"
+import mallHeadBackground from "../img/homeHead.jpg";
+import shopBackground from "../img/pcc.jpg";
 
 function Mall() {
     return (
@@ -9,6 +10,13 @@ function Mall() {
                 <div className="mallHeadText">
                     <h1>Kennetic Cloud Mall</h1>
                     <p>Original art from a variety of artists, in a wide array of styles and genres. Printed on vast catalog of products ready to deliver to your home.</p>
+                </div>
+            </div>
+            <div className="mallBodyWrap">
+                <div class="hex">
+                    <div class="hex-background">
+                        <img src={shopBackground} />
+                    </div>
                 </div>
             </div>
         </div>
