309094de79e9dd9d5fa41e6722498db5ea1609d4
Kenneth Brook <kenneth.brook@hotmail.com>
2022-09-30 13:00:46 -0400

    home header created

--- frontend/src/mall-home/Mall.js
+++ frontend/src/mall-home/Mall.js
@@ -1,9 +1,16 @@
 import React from "react";
 
+import mallHeadBackground from "../img/homeHead.jpg"
+
 function Mall() {
     return (
-        <div>
-            <h1>Mall Home Page</h1>
+        <div className="mallWrap">
+            <div className="mallHead" style={{ backgroundImage: `url(${mallHeadBackground})` }}>
+                <div className="mallHeadText">
+                    <h1>Kennetic Cloud Mall</h1>
+                    <p>Original art from a variety of artists, in a wide array of styles and genres. Printed on vast catalog of products ready to deliver to your home.</p>
+                </div>
+            </div>
         </div>
     );
 }
